6d99f47e1507ab3ad629c27ec81a0281b753afa0
Jú Gabriele <127549501+Juli-Gabriele@users.noreply.github.com>
2025-04-21 16:55:32 -0300

    Update 1051 - Imposto de Renda.cpp

--- 1051 - Imposto de Renda.cpp
+++ 1051 - Imposto de Renda.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
+int main(){
+ double valor,res1,res2,res3,res4,res5,result;
+ cin >> valor;
+ if ( valor >= 0 && valor <= 2000.0){
+    cout << ("Isento") << endl;
+}
+ if ( valor > 2000.0 && valor <= 3000.0){
+      res1 = (valor - 2000);
+      result = res1 *0.08 ;
+      cout.precision(2);
+      cout.setf(ios::fixed);
+      cout << ("R$ ") << result << endl;
+}
+ if ( valor > 3000.0 && valor <= 4500){
+     res1 = (valor - 2000);
+     res2 =  (valor - 3000);
+     res3 = res1 - res2;
+     result = ((res3* 0.08) + (res2 *0.18));
+     cout.precision(2);
+     cout.setf(ios::fixed);
+     cout << ("R$ ") << result << endl;
+}
+  if ( valor > 4500){
+    res1 = (valor - 2000);
+    res2 = (valor - 3000);
+    res3 = (valor - 4500);
+    res4 = (res1 - res2);
+    res5 = (res2- res3);
+    result = ((res4* 0.08) + (res5 *0.18) + (res3 *0.28));
+    cout.precision(2);
+    cout.setf(ios::fixed);
+    cout << ("R$ ") << result << endl;
+
+}
+
+}
+
 
