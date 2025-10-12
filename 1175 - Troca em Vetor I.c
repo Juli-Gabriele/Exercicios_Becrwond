1ff85bfd6371d806504d0a7fcad036323576a0da
Jú Gabriele <127549501+Juli-Gabriele@users.noreply.github.com>
2025-10-12 16:22:43 -0300

    Update 1175 - Troca em Vetor I.c

--- 1175 - Troca em Vetor I.c
+++ 1175 - Troca em Vetor I.c
@@ -17,3 +17,29 @@ int main(){
  return 0;
 
 }
+
+//Outra possibilidade
+
+#include <stdio.h>
+#define Tam 5
+int main (){
+  int i,j,v[Tam];
+
+   for (int i= 0; i < Tam ; i++){
+        scanf("%d",&v[i]);
+   }
+    for (i = 0,j = Tam - 1; i < j; i++,j--) {
+            int x = v[i];
+             v[i] = v[j];
+              v[j] = x;
+    }
+
+    for (i = 0; i < Tam; i++){
+        printf("N[%d] = %d\n", i, v[i]);
+    }
+
+
+
+ return 0;
+}
+
