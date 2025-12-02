b2d3f63a4fe6200415185b84f271ec6f49231574
Jú Gabriele <127549501+Juli-Gabriele@users.noreply.github.com>
2025-12-01 23:40:53 -0300

    Create  1190 - Área Direita.c

---
+++ 1190 - Área Direita.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define Tam 12
+
+int main(){
+   int i,j;
+   char op;
+   double soma = 0, q = 0, m[Tam][Tam];
+
+    scanf("%c",&op);
+    for (i = 0; i < Tam ; i++){
+       for (j = 0; j < Tam; j++){
+            scanf("%lf",&m[i][j]);
+          if (( i < j) && (i + j >= Tam )){
+            soma += m[i][j];
+            q++;
+        }
+      }
+
+   }
+     if (op == 'M'){
+        printf("%0.1lf\n",soma/q);
+     }else {
+
+        printf("%0.1lf\n",soma);
+     }
+
+
+
+
+
+    return 0;
+}
+
+
+
